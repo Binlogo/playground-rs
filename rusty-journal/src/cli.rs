b977d873d3ee5bec76a8fdb0f3523073f7aeaca7
Binlogo <binboy@live.com>
2021-05-01 23:22:26 +0800

    Add rusty-journal: cli module

---
+++ rusty-journal/src/cli.rs
@@ -0,0 +1,28 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    /// Add a task
+    Add {
+        /// Task description
+        #[structopt()]
+        task: String,
+    },
+    /// Complete a task, remove by position
+    Done {
+        #[structopt()]
+        position: usize,
+    },
+    /// List all tasks
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Rusty journal", about = "A command line todo app")]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
---
+++ rusty-journal/src/main.rs
@@ -0,0 +1,7 @@
+mod cli;
+use structopt::StructOpt;
+
+fn main() {
+    let args = cli::CommandLineArgs::from_args();
+    println!("{:#?}", args);
+}
