504adf4d330f5ca4363291ca965eb6f5cd5a3d08
Binlogo <binboy@live.com>
2021-05-02 13:25:50 +0800

    Rusty journal: impl complete task

--- rusty-journal/src/tasks.rs
+++ rusty-journal/src/tasks.rs
@@ -1,8 +1,8 @@
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::{Deserialize, Serialize};
-use std::fs::OpenOptions;
 use std::io::{BufReader, Result, Seek, SeekFrom};
 use std::path::PathBuf;
+use std::{fs::OpenOptions, io::Error};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -24,25 +24,42 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
         .write(true)
         .create(true)
         .open(journal_path)?;
-
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(err) if err.is_eof() => Vec::new(),
-        Err(err) => Err(err)?,
-    };
-
-    file.seek(SeekFrom::Start(0))?;
-
+    let mut tasks = parse_tasks(&mut file)?;
     tasks.push(task);
     serde_json::to_writer(file, &tasks)?;
-
     Ok(())
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
-    unimplemented!()
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+    let mut tasks = parse_tasks(&mut file)?;
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Invaid task id",
+        ));
+    }
+    tasks.remove(task_position - 1);
+
+    file.set_len(0)?;
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
 }
 
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
     unimplemented!()
+}
+
+fn parse_tasks(file: &mut std::fs::File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?;
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&*file) {
+        Ok(tasks) => tasks,
+        Err(err) if err.is_eof() => Vec::new(),
+        Err(err) => Err(err)?,
+    };
+    file.seek(SeekFrom::Start(0))?;
+    Ok(tasks)
 }
